a4f55e91006402e4d5cadc07abb237c598e98b3c
bubbybumble <bubbybumble@gmail.com>
2023-10-07 19:40:18 -0400

    changed dependencies

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 #just installs some dependencies. I do apt since this is for a pi but I'm currently using this on fedora and it works
 
-sudo apt install figlet sox xdotool -y
+sudo apt install figlet sox -y
