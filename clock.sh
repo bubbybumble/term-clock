0c3d7de37d20cce89376197f25922536a922f35f
bubbybumble <bubbybumble@gmail.com>
2023-10-07 19:22:09 -0400

    removed seconds; made only refresh when minute changes to prevent flickering

--- clock.sh
+++ clock.sh
@@ -12,29 +12,41 @@ tput civis
 TIMER=240 
 DURATION=240
 
-DIVIDER="\n\n" # Divider between displayed info
+MINUTE=-1
+
+DIVIDER="\n--------------------------------------------------------------------------------\n" # Divider between displayed info, 80 chars
 
 
 while true 
 do 
-  tput clear
-  date +"%H : %M : %S" | figlet
-  echo -e $DIVIDER
-  date +"%A, %D"
-  echo -e $DIVIDER
-  if [ "$TIMER" -ge "$DURATION" ]
+  CURRENT_MINUTE=$(date +"%M")
+  if [ "$CURRENT_MINUTE" != "$MINUTE" ]
   then
-    TIMER=0
-    URL=$(curl -s "https://api.weather.gov/points/35.227,-80.8431" | jq -r '.properties.forecast')
-    DATA=$(curl -s "$URL" | jq -r '.properties.periods[0]')
-    FORECAST=$(echo $DATA | jq -r '.detailedForecast')
-    TEMPERATURE=$(echo $DATA | jq -r '.temperature')
-  else
-    ((TIMER++))
+    MINUTE=$CURRENT_MINUTE
+
+    tput clear
+    date +"%H : %M" | figlet
+    echo -e $DIVIDER
+    date +"%A, %D"
+    echo -e $DIVIDER
+
+    if [ "$TIMER" -ge "$DURATION" ]
+    then
+      TIMER=0
+      URL=$(curl -s "https://api.weather.gov/points/35.227,-80.8431" | jq -r '.properties.forecast')
+      DATA=$(curl -s "$URL" | jq -r '.properties.periods[0]')
+      FORECAST=$(echo $DATA | jq -r '.detailedForecast')
+      TEMPERATURE=$(echo $DATA | jq -r '.temperature')
+    else
+      ((TIMER++))
+    fi
+    echo "$TEMPERATURE F"
+    echo -e $DIVIDER
+    echo $FORECAST
+
   fi
-  echo "$TEMPERATURE F"
-  echo -e $DIVIDER
-  echo $FORECAST
+  
+
   sleep 1
 done
 
